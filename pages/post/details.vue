358cd826d6fff103b4d893ccf6983dd0eaacbd9b
qianwenru <1094472706@qq.com>
2019-09-14 10:31:54 +0800

    攻略详情页静态页面完成

---
+++ pages/post/details.vue
@@ -0,0 +1,273 @@
+<template>
+  <div class="details">
+    <el-container>
+      <el-main class="main" justify="spance-between">
+        <div class="container">
+          <!-- 面包屑 -->
+          <el-breadcrumb separator="/">
+            <el-breadcrumb-item :to="{ path: '/' }">旅游攻略</el-breadcrumb-item>
+            <el-breadcrumb-item>旅游攻略</el-breadcrumb-item>
+          </el-breadcrumb>
+          <!-- 主题 -->
+          <h1 class="title">
+            <nuxt-link to="#">塞班贵？一定是你的打开方式不对！6000块玩转塞班</nuxt-link>
+          </h1>
+          <div class="info">
+            <span>攻略：2019-05-22 10:57</span>
+            <span>阅读：7298</span>
+          </div>
+
+
+          <div class="content">
+            <p>大家对塞班岛总存在着这样的误解，知道它是美属地盘，就理所当然地觉得这里的花费一定很高，花费高有高的玩法，那如果只有6000块的预算呢？要怎么玩？关于旅行这件事，我们要让钱花得更有道理，收下这份攻略，带你6000块花式玩转塞班。</p>
+            <p>
+              <span style="background-color: rgb(252, 242, 220);">
+                <img
+                  src="https://n3-q.mafengwo.net/s10/M00/E8/E4/wKgBZ1octoCABhgLAAafahORRLs91.jpeg?imageView2%2F2%2Fw%2F1360%2Fq%2F90"
+                />
+              </span>
+              <span style="color: rgb(153, 153, 153);">图：塞班岛。 by第5季旅游</span>
+            </p>
+            <h2>一、怎样用6000块玩转塞班？</h2>
+            <p>大多数出境游客人不做预算或最终花费远远超出预算，对预算的合理分配对控制我们旅行的花费就很重要了，如何只花6000块玩转塞班岛，对预算超支say&nbsp;no？下面从5个方面来为您一一解读：机票+酒店、岛上交通、玩乐项目、吃以及购买纪念品。</p>
+            <p>
+              <br />
+            </p>
+            <h3>1.怎样订机票+酒店性价比最高？</h3>
+            <p>
+              机票和酒店的花销往往会占据我们旅行大半的花销，假设机票酒店为塞班行预算的一半，剩下的吃行玩购为预算的另一半，如果能在机票酒店这部分省下钱，也就意味着在塞班岛用来吃行玩购的钱就增加了
+              <span>
+                <img
+                  src="https://images.mafengwo.net/images/i/face/brands_v3/6@2x.png"
+                />
+              </span>
+            </p>
+            <p>
+              <br />
+            </p>
+            <p>
+              <span style="background-color: rgb(252, 242, 220);">
+                <img
+                  src="https://p1-q.mafengwo.net/s10/M00/E9/33/wKgBZ1octwiAAKAoAAJ9ixcJc9M71.jpeg?imageView2%2F2%2Fw%2F1360%2Fq%2F90"
+                />
+              </span>
+            </p>
+            <p>
+              <br />
+            </p>
+            <p>怎样去塞班？可以转机也可以直飞，转机大多会从韩国转，提前蹲守能买到韩国飞塞班的特价机票，2000以下就能入手，加上国内飞韩国的机票来回塞班得5000+，还没算上在塞班的住宿费用，转机还有中途等待的时间，光花在路途上的时间就比直飞要多上一倍甚至更多，转乘奔波劳累，非联程票还要担心行李托运问题，所以建议大家有直飞还是尽量选择直飞。</p>
+            <p>在酒店上，旅途中我们呆在酒店的时间远比在外游玩的时间少，酒店干净整洁基本就能满足我们休息的需求，塞班不是个享受酒店的地方而且还真不能跟国内星级酒店等位比较，所以不建议大家花过多的钱在塞班的酒店体验上。</p>
+            <p>怎样在机票酒店上获得最高性价比的体验？&nbsp;直飞塞班的航班一般和酒店一起打包成机票+酒店套餐，价格要比单定机票、酒店要更加便捷实惠，往往3千多就能把机票和酒店一键搞定。</p>
+          </div>
+
+           <el-row type="flex" justify="center" >
+             <div class="ctrl-item">
+               <i class="iconfont_iconpinglun"></i>
+               <p>评论(100)</p>
+              </div> 
+              <div class="ctrl-item">
+               <i class="icon_iconstarl"></i>
+               <p>收藏</p>
+              </div> 
+              <div class="ctrl-item">
+               <i class="iconfont_iconfenxiang"></i>
+               <p>分享</p>
+              </div> 
+              <div class="ctrl-item">
+               <i class="iconfont_iconding"></i>
+               <p>点赞(51)</p>
+              </div> 
+           </el-row>
+        </div>
+      </el-main>
+
+
+
+      <el-aside width="280px">
+        <div class="aside">
+          <h4>相关攻略</h4>
+          <nuxt-link to="/" style="dispaly:block">
+            <div class="post-imgText">
+              <div class="image">
+                <img
+                  src="http://157.122.54.189:9095/uploads/da193301bd7c48b099725b0c75b8429b.jpg"
+                  alt
+                />
+              </div>
+              <div class="text">
+                <h3>北京</h3>
+                <p>2019-09-12 9:13 阅读 1</p>
+              </div>
+            </div>
+          </nuxt-link>
+          <nuxt-link to="/" style="dispaly:block">
+            <div class="post-imgText">
+              <div class="image">
+                <img
+                  src="http://157.122.54.189:9095/uploads/da193301bd7c48b099725b0c75b8429b.jpg"
+                  alt
+                />
+              </div>
+              <div class="text">
+                <h3>北京</h3>
+                <p>2019-09-12 9:13 阅读 1</p>
+              </div>
+            </div>
+          </nuxt-link>
+          <nuxt-link to="/" style="dispaly:block">
+            <div class="post-imgText">
+              <div class="image">
+                <img
+                  src="http://157.122.54.189:9095/uploads/da193301bd7c48b099725b0c75b8429b.jpg"
+                  alt
+                />
+              </div>
+              <div class="text">
+                <h3>北京</h3>
+                <p>2019-09-12 9:13 阅读 1</p>
+              </div>
+            </div>
+          </nuxt-link>
+          <nuxt-link to="/" style="dispaly:block">
+            <div class="post-imgText">
+              <div class="image">
+                <img
+                  src="http://157.122.54.189:9095/uploads/da193301bd7c48b099725b0c75b8429b.jpg"
+                  alt
+                />
+              </div>
+              <div class="text">
+                <h3>北京</h3>
+                <p>2019-09-12 9:13 阅读 1</p>
+              </div>
+            </div>
+          </nuxt-link>
+          <nuxt-link to="/" style="dispaly:block">
+            <div class="post-imgText">
+              <div class="image">
+                <img
+                  src="http://157.122.54.189:9095/uploads/da193301bd7c48b099725b0c75b8429b.jpg"
+                  alt
+                />
+              </div>
+              <div class="text">
+                <h3>北京</h3>
+                <p>2019-09-12 9:13 阅读 1</p>
+              </div>
+            </div>
+          </nuxt-link>
+        </div>
+      </el-aside>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+.el-container {
+  width: 1000px;
+  margin: 0 auto;
+}
+.main {
+  width: 700px;
+  padding: 20px 0;
+  margin-right: 20px;
+  .container {
+    .title {
+      width: 700px;
+      padding: 20px 0;
+      border-bottom: 1px solid #ddd;
+      font-weight: bold;
+      font-size: 32px;
+    }
+  }
+}
+.info {
+  display: block;
+  color: #999;
+  text-align: right;
+  padding: 20px;
+  font-size: 16px;
+  &:first-child {
+    margin-right: 20px;
+  }
+}
+.content {
+  // font-size:18px;
+  text-align: left;
+  // margin-top:25px;
+  // margin-bottom:25px;
+  margin: 0 auto;
+  width: 700px;
+  img {
+    width:100%;
+    height:100%;
+    margin: 10px 0;
+    // &:nth-child(2){
+    //   width: 16px;
+    // }
+  }
+}
+
+
+.el-aside {
+  background-color: #fff;
+  color: #333;
+  // text-align: center;
+  // line-height: 200px;
+}
+
+.aside {
+  width: 280px;
+  h4 {
+    font-weight: 400;
+    font-size: 18px;
+    padding: 20px 0;
+    border-bottom: 1px solid #ddd;
+  }
+  .post-imgText {
+    border-bottom: 1px solid #ddd;
+    display: flex;
+    padding: 20px 0;
+    .text {
+      width: 170px;
+      position: relative;
+      p {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        font-size: 12px;
+        color: #999;
+      }
+      h3 {
+        line-height: 1.4em;
+        height: 2.8em;
+        overflow: hidden;
+        position: absolute;
+        left: 0;
+        top: 0;
+        width: 100%;
+        font-weight: normal;
+        font-size: 16px;
+      }
+    }
+    .image {
+      width: 100px;
+      height: 80px;
+      flex-shrink: 0;
+      overflow: hidden;
+      margin-right: 10px;
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
+}
+
+// 1.基本信息（我是****，我来自哪里） 2.工作经历，工作经验 （时间周期，作品介绍）
+// 3.框架的介绍（技术栈，离职原因（因为一些客观的原因，迫不得已离职，或者因为个人的事情（有可能会问具体什么原因））
+</style>
---
+++ pages/post/details2.vue
@@ -0,0 +1,226 @@
+<template>
+  <div class="details2">
+    <el-container>
+      <el-main class="main" justify="spance-between">
+        <div class="container">
+          <!-- 面包屑 -->
+          <el-breadcrumb separator="/">
+            <el-breadcrumb-item :to="{ path: '/' }">旅游攻略</el-breadcrumb-item>
+            <el-breadcrumb-item>旅游攻略</el-breadcrumb-item>
+          </el-breadcrumb>
+          <!-- 主题 -->
+          <h1 class="title">
+            <nuxt-link to="#">远东行：用好奇心打量这座城 —— 最值得收藏的海参崴出行攻略</nuxt-link>
+          </h1>
+          <div class="info">
+            <span>攻略：2019-05-22 12:59</span>
+            <span>阅读：1110</span>
+          </div>
+          <div class="content">
+            <p>
+              <span style="color: rgb(68, 68, 68);">想象一下一个距离&nbsp;</span>
+              <a
+                href="https://www.mafengwo.cn/travel-scenic-spot/mafengwo/10065.html"
+                target="_blank"
+                style="color: rgb(77, 77, 77);"
+              >北京</a>
+              <span style="color: rgb(68, 68, 68);">&nbsp;只有2.5小时飞行距离的城市：身处&nbsp;</span>
+              <a
+                href="https://www.mafengwo.cn/travel-scenic-spot/mafengwo/52314.html"
+                target="_blank"
+                style="color: rgb(77, 77, 77);"
+              >亚洲</a>
+              <span
+                style="color: rgb(68, 68, 68);"
+              >&nbsp;却能感受到十足的欧陆风情——欧式建筑和街道，金发碧眼的路人，正宗的西餐外加只有国内一半售价的帝王蟹可以敞开吃——更难能可贵的是，这里对国人（实质）免签，有直飞航班，低廉的物价，且尚未有太多的游客涉足还保留着原汁原味的传统风情！</span>
+            </p>
+            <p>
+              <span style="color: rgb(68, 68, 68);">
+                <span class="ql-cursor"></span>
+              </span>
+            </p>
+            <p>
+              <img
+                src="https://p3-q.mafengwo.net/s13/M00/41/C4/wKgEaVyaOs2AA9IKAAj8Lg2YzaU64.jpeg?imageMogr2%2Fthumbnail%2F1360x%2Fstrip%2Fquality%2F90"
+              />
+            </p>"
+          </div>
+        </div>
+      </el-main>
+
+      <el-aside width="280px">
+        <div class="aside">
+          <h4>相关攻略</h4>
+          <nuxt-link to="/" style="dispaly:block">
+            <div class="post-imgText">
+              <div class="image">
+                <img
+                  src="http://157.122.54.189:9095/uploads/da193301bd7c48b099725b0c75b8429b.jpg"
+                  alt
+                />
+              </div>
+              <div class="text">
+                <h3>北京</h3>
+                <p>2019-09-12 9:13 阅读 1</p>
+              </div>
+            </div>
+          </nuxt-link>
+          <nuxt-link to="/" style="dispaly:block">
+            <div class="post-imgText">
+              <div class="image">
+                <img
+                  src="http://157.122.54.189:9095/uploads/da193301bd7c48b099725b0c75b8429b.jpg"
+                  alt
+                />
+              </div>
+              <div class="text">
+                <h3>北京</h3>
+                <p>2019-09-12 9:13 阅读 1</p>
+              </div>
+            </div>
+          </nuxt-link>
+          <nuxt-link to="/" style="dispaly:block">
+            <div class="post-imgText">
+              <div class="image">
+                <img
+                  src="http://157.122.54.189:9095/uploads/da193301bd7c48b099725b0c75b8429b.jpg"
+                  alt
+                />
+              </div>
+              <div class="text">
+                <h3>北京</h3>
+                <p>2019-09-12 9:13 阅读 1</p>
+              </div>
+            </div>
+          </nuxt-link>
+          <nuxt-link to="/" style="dispaly:block">
+            <div class="post-imgText">
+              <div class="image">
+                <img
+                  src="http://157.122.54.189:9095/uploads/da193301bd7c48b099725b0c75b8429b.jpg"
+                  alt
+                />
+              </div>
+              <div class="text">
+                <h3>北京</h3>
+                <p>2019-09-12 9:13 阅读 1</p>
+              </div>
+            </div>
+          </nuxt-link>
+          <nuxt-link to="/" style="dispaly:block">
+            <div class="post-imgText">
+              <div class="image">
+                <img
+                  src="http://157.122.54.189:9095/uploads/da193301bd7c48b099725b0c75b8429b.jpg"
+                  alt
+                />
+              </div>
+              <div class="text">
+                <h3>北京</h3>
+                <p>2019-09-12 9:13 阅读 1</p>
+              </div>
+            </div>
+          </nuxt-link>
+        </div>
+      </el-aside>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+.el-container {
+  width: 1000px;
+  margin: 0 auto;
+}
+.main {
+  width: 700px;
+  padding: 20px 0;
+  margin-right: 20px;
+  .container {
+    .title {
+      width: 700px;
+      padding: 20px 0;
+      border-bottom: 1px solid #ddd;
+      font-weight: bold;
+      font-size: 32px;
+    }
+  }
+}
+.info {
+  display: block;
+  color: #999;
+  text-align: right;
+  padding: 20px;
+  font-size: 16px;
+  &:first-child {
+    margin-right: 20px;
+  }
+}
+.content {
+  text-align: left;
+  margin: 0 auto;
+  width: 700px;
+  img {
+    width: 100%;
+    height: 100%;
+    margin: 10px 0;
+  }
+}
+
+.el-aside {
+  background-color: #fff;
+  color: #333;
+}
+
+.aside {
+  width: 280px;
+  h4 {
+    font-weight: 400;
+    font-size: 18px;
+    padding: 20px 0;
+    border-bottom: 1px solid #ddd;
+  }
+  .post-imgText {
+    border-bottom: 1px solid #ddd;
+    display: flex;
+    padding: 20px 0;
+    .text {
+      width: 170px;
+      position: relative;
+      p {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        font-size: 12px;
+        color: #999;
+      }
+      h3 {
+        line-height: 1.4em;
+        height: 2.8em;
+        overflow: hidden;
+        position: absolute;
+        left: 0;
+        top: 0;
+        width: 100%;
+        font-weight: normal;
+        font-size: 16px;
+      }
+    }
+    .image {
+      width: 100px;
+      height: 80px;
+      flex-shrink: 0;
+      overflow: hidden;
+      margin-right: 10px;
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
+}
+</style>
